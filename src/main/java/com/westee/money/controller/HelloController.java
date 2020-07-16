2597f65d1b909f6f4cbf80cae4112c12f257f2a4
westee <904885508@qq.com>
2020-07-16 22:52:10 +0800

    使用Swagger

--- src/main/java/com/westee/money/controller/HelloController.java
+++ src/main/java/com/westee/money/controller/HelloController.java
@@ -1,15 +1,24 @@
 package com.westee.money.controller;
 
 import com.westee.money.model.service.Greeting;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
+@Api(value = "Hello Controller")
 public class HelloController {
 
     private AtomicLong counter = new AtomicLong();
 
+    @ApiOperation(value = "测试", response = String.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Hello Controller")
+    })
     @GetMapping("v1.0/greeting")
     public Greeting sayHello(@RequestParam("name") String name){
         return new Greeting(counter.incrementAndGet(), name);
